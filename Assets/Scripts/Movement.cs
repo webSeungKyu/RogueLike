20cd2ccbd8b2198113a93f8a7eb9aa14a913d42f
webSeungKyu <abcsksk1@gmail.com>
2024-04-19 10:56:21 +0900

    AI 활용하여 Idle, Move, Die 애니메이션 구현

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,19 +8,21 @@ public class Movement : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer; // SpriteRenderer 컴포넌트 참조
+    private Animator animator; // Animator 컴포넌트 참조
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Rigidbody2D 컴포넌트 가져오기
         spriteRenderer = GetComponent<SpriteRenderer>(); // SpriteRenderer 컴포넌트 가져오기
+        animator = GetComponent<Animator>(); // Animator 컴포넌트 가져오기
 
     }
 
 
     void Update()
     {
-
+        
     }
     void FixedUpdate()
     {
@@ -43,7 +45,23 @@ public class Movement : MonoBehaviour
         {
             spriteRenderer.flipX = false; // 오른쪽으로 이동하면 오브젝트를 원래대로 복원
         }
+
+        // 이동 상태에 따라 애니메이션 재생
+        if (movement.magnitude > 0)
+        {
+            animator.SetBool("isMoving", true); // 이동 중이면 isMoving 매개변수를 true로 설정하여 Move 애니메이션 재생
+        }
+        else
+        {
+            animator.SetBool("isMoving", false); // 정지 상태면 isMoving 매개변수를 false로 설정하여 Idle 애니메이션 재생
+        }
     }
 
-    
+    public void Die()
+    {
+        // 죽음 상태 애니메이션을 재생하기 위해 Animator에 죽음 상태를 알리는 매개변수를 설정
+        animator.SetTrigger("Die");
+    }
+
+
 }
