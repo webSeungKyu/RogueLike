ff00a4386c49c863e4cd4277943977b54cefd619
webSeungKyu <abcsksk1@gmail.com>
2024-04-22 17:27:50 +0900

    Invoke()는 게임의 프레임 속도에 영향을 받지 않고 고정된 시간 간격으로 호출되므로 수정

--- Assets/Scripts/BulletShot.cs
+++ Assets/Scripts/BulletShot.cs
@@ -6,17 +6,24 @@ public class BulletShot : MonoBehaviour
 {
     public GameObject bullet;
     float shotCoolTime = 1f;
-    
-    void Start()
-    {
+    float timer = 0f;
 
-        InvokeRepeating("BulletInstantiate", 4f, shotCoolTime);
+    private void FixedUpdate()
+    {
+        // 타이머 업데이트
+        timer += Time.fixedDeltaTime;
+
+        // 일정 간격마다 총알 생성
+        if (timer >= shotCoolTime)
+        {
+            BulletInstantiate();
+            // 타이머 초기화
+            timer = 0f;
+        }
     }
 
     void BulletInstantiate()
     {
         Instantiate(bullet, transform.position, Quaternion.identity);
     }
-
-
 }
